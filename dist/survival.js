b274c8ed2d09d7441cd8e6ee80ec5e20b6979805
inagaki <h.inagaki@hiyoku.co.jp>
2024-01-12 11:24:35 +0900

    readonlyは入れ子構造の場合、子には影響しない。

--- dist/survival.js
+++ dist/survival.js
@@ -55,27 +55,55 @@
 // // calc.sum = null;
 // calc.sum(1, 5); // 上でnull代入しているからメソッドはnullになってしまっている
 /* オブジェクトの型注釈 */
-const box = {
-    width: 100,
-    height: 500
+// const box: {
+//   width: number;
+//   height: number;
+// } = {
+//   width: 100,
+//   height: 500
+// };
+// // 型エイリアスもOK
+// type Box = {
+//   width: number;
+//   height: number;
+// };
+// const box2: Box = {
+//   width: 2,
+//   height: 5
+// };
+// // メソッドの型注釈
+// let calc: {
+//   sum(x: number, y: number): number;
+// }
+// // これもOK
+// let calc2: {
+//   sum: (x: number, y: number) => number
+// };
+// calc = {
+//   sum(x, y) {
+//     return x + y;
+//   }
+// };
+// // Record<Key, Type>
+// // 連想配列のようなキーバリューのオブジェクトを定義する場合、ユーティリティ型のRecordを使う方法もある
+// let foo: Record<string, number>;
+// foo = {
+//   a: 1,
+//   b: 2
+// };
+/* オブジェクトの型のreadonlyプロパティ */
+// let obj: {
+//   readonly foo: number;
+// };
+// obj = { foo: 1 };
+// obj.foo = 2; // Cannot assign to 'foo' because it is a read-only property.
+// readonlyが指定されたプロパティのみが読み取り専用になる。
+// 入れ子になっている場合、その入れ子までをreadonlyにする機能はない
+let obj;
+obj = {
+    foo: {
+        bar: 5,
+    },
 };
-const box2 = {
-    width: 2,
-    height: 5
-};
-// メソッドの型注釈
-let calc;
-// これもOK
-let calc2;
-calc = {
-    sum(x, y) {
-        return x + y;
-    }
-};
-// Record<Key, Type>
-// 連想配列のようなキーバリューのオブジェクトを定義する場合、ユーティリティ型のRecordを使う方法もある
-let foo;
-foo = {
-    a: 1,
-    b: 2
-};
+// obj.foo = { bar: 1 }; // Cannot assign to 'foo' because it is a read-only property.
+obj.foo.bar = 1; // 入れ子を直接書き換えるのはOK
