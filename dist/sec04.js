2f6e7650360f5a0ae9f4ee28676b9c86445decb1
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-13 23:33:14 +0900

    返り値がない場合はvoid型

--- dist/sec04.js
+++ dist/sec04.js
@@ -3,17 +3,26 @@
   section4 typescript function
  ========================== */
 /* 4.1.1 関数宣言で関数を作る */
-//  function 関数名(引数): 返り値の型 {}
-function range(min, max) {
-    const result = [];
-    for (let i = min; i <= max; i++) {
-        result.push(i);
+// //  function 関数名(引数): 返り値の型 {}
+//  function range(min: number, max: number): number[] {
+//   const result = [];
+//   for (let i = min; i <= max; i++) {
+//     result.push(i);
+//   }
+//   return result;
+//  }
+//  console.log(range(1,10));
+// // 関数宣言はhoistingが起こる。
+// // 関数宣言前に関数呼び出ししてもエラーにならない。これがホイスティング（巻き上げ）
+// console.log(hoisting(1,9));
+// function hoisting(x: number, y: number): number {
+//   return x + y;
+// }
+/* 4.1.2 返り値のない関数 */
+// 返り値がない関数、例えばconsoleに表示させるだけの関数だったら返り値は void を指定する
+function greetingLoop(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("Hello");
     }
-    return result;
 }
-console.log(range(1, 10));
-// 関数宣言はhoistingが起こる。
-console.log(hoisting(1, 9));
-function hoisting(x, y) {
-    return x + y;
-}
+greetingLoop(10);
--- src/sec04.ts
+++ src/sec04.ts
@@ -3,21 +3,30 @@
  ========================== */
 
  /* 4.1.1 関数宣言で関数を作る */
-//  function 関数名(引数): 返り値の型 {}
- function range(min: number, max: number): number[] {
-  const result = [];
-  for (let i = min; i <= max; i++) {
-    result.push(i);
-  }
-  return result;
- }
+// //  function 関数名(引数): 返り値の型 {}
+//  function range(min: number, max: number): number[] {
+//   const result = [];
+//   for (let i = min; i <= max; i++) {
+//     result.push(i);
+//   }
+//   return result;
+//  }
 
- console.log(range(1,10));
+//  console.log(range(1,10));
 
-// 関数宣言はhoistingが起こる。
-// 関数宣言前に関数呼び出ししてもエラーにならない。これがホイスティング（巻き上げ）
-console.log(hoisting(1,9));
+// // 関数宣言はhoistingが起こる。
+// // 関数宣言前に関数呼び出ししてもエラーにならない。これがホイスティング（巻き上げ）
+// console.log(hoisting(1,9));
 
-function hoisting(x: number, y: number): number {
-  return x + y;
+// function hoisting(x: number, y: number): number {
+//   return x + y;
+// }
+
+/* 4.1.2 返り値のない関数 */
+// 返り値がない関数、例えばconsoleに表示させるだけの関数だったら返り値は void を指定する
+function greetingLoop(n: number): void {
+  for (let i = 0; i < n; i++) {
+    console.log("Hello");
+  }  
 }
+greetingLoop(10);
