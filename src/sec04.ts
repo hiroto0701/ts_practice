f5c7aba0f69c3fdbb765cb72f2b94fc96631a17a
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-14 21:54:48 +0900

    p.149 コールバック関数

--- src/sec04.ts
+++ src/sec04.ts
@@ -134,3 +134,39 @@
 // const nums3: [number, number, number] = [1, 2, 3];
 // console.log(sum2(...nums2)); // error TS2556: A spread argument must either have a tuple type or be passed to a rest parameter.
 // console.log(sum2(...nums3));
+
+/* 4.1.10 コールバック関数 */
+// 関数の引数として関数を渡すことで、引数として渡される関数のことをコールバック関数という。
+type User = { name: string, age: number };
+const getName = (u: User): string => u.name;
+const users: User[] = [
+  { name: "hiroto", age: 25 },
+  { name: "taka", age: 35 },
+];
+const names = users.map(getName);
+console.log(names);
+
+// 省略型
+const users2: User[] = [
+  { name: "hiroto", age: 25 },
+  { name: "taka", age: 35 },
+];
+const names2 = users2.map((u: User): string => u.name);
+console.log(names2);
+
+// 他の配列メソッド（高階関数）
+// 指定された配列の中から指定された関数で実装されているテストに合格した要素だけを抽出したシャローコピーを作成
+const adultUsers = users.filter((u: User) => u.age >= 20);
+console.log(adultUsers);
+
+// 列内のすべての要素が指定された関数で実装されたテストに合格するかどうかをテストします。これは論理値を返します。
+const allAdult = users.every((u: User) => u.age >= 20);
+console.log(allAdult);
+
+// some() は Array インスタンスのメソッドで、指定された関数で実装されているテストに、配列の中の少なくとも 1 つの要素が合格するかどうかを判定します。
+const seniorExists = users.some((u :User) => u.age >= 60);
+console.log(seniorExists);
+
+// find() は Array インスタンスのメソッドで、提供されたテスト関数を満たす配列内の最初の要素を返します。 テスト関数を満たす値がない場合は、 undefined を返します。
+const taka = users.find((u: User) => u.name.startsWith("taka"));
+console.log(taka);
