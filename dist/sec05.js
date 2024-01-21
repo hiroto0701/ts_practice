5ff6f2a4af397587a46b42898625131db1ce6f33
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-21 22:22:40 +0900

    p.192 staticなプロパティ、メソッド。インスタンスを作成してアクセスはNGだが、直接参照は可能。つまりstaticはそのクラスそのものに属するプロパティ、メソッドである。

--- dist/sec05.js
+++ dist/sec05.js
@@ -26,17 +26,48 @@
 // const readOnlyName = new readOnly();
 // readOnlyName.name = "aaaaa"  // コンパイルエラー Cannot assign to 'name' because it is a read-only property.
 /* 5.1.4 constructor */
-// インスタンスが生成される際に呼び出される処理
-// 主な使い道はインスタンスのプロパティの初期化
+// // インスタンスが生成される際に呼び出される処理
+// // 主な使い道はインスタンスのプロパティの初期化
+// class User {
+//   name: string;
+//   age: number;
+//   readonly isMen: boolean; 
+//   constructor(name: string, age: number, isMen: boolean) {
+//     this.name = name;
+//     this.age = age;
+//     // construct内ではreadonlyのプロパティに代入OK
+//     // readonlyプロパティは「いったんオブジェクトができてしまったら変更不可」なので
+//     // constructorはまだオブジェクトの生成中、だからOKという理解
+//     this.isMen = isMen;
+//   }
+//   // method
+//   isAdult(): boolean {
+//     return this.age >= 20;
+//   }
+// }
+// const me = new User("hiroto", 25, true);
+// console.log(me.name, me.age, me.isMen, me.isAdult());  // hiroto 25 true
+/* 5.1.5 静的プロパティ、静的メソッド */
+// staticはインスタンスではなく、クラスそのものに属するプロパティ・メソッドのこと
+// Userクラスは staticな変数（adminName）と staticな関数（getAdminUser()）を持っている
 class User {
+    static getAdminUser() {
+        return new User(User.adminName, 25);
+    }
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
-    // method
     isAdult() {
         return this.age >= 20;
     }
 }
-const me = new User("hiroto", 25);
-console.log(me.name, me.age, me.isAdult()); // hiroto 25 [Function: isAdult]
+User.adminName = "inagaki";
+// インスタンスを作成するのではなく、直接UserクラスのstaticなプロパティとメソッドにアクセスするのはOK
+console.log(User.adminName);
+const admin = User.getAdminUser();
+console.log(admin.age);
+console.log(admin.name);
+// インスタンスを作成して、staticなプロパティや、メソッドにアクセスするのはNG
+const hiro = new User("hiro", 25);
+// console.log(hiro.adminName);  // Property 'adminName' does not exist on type 'User'. Did you mean to access the static member 'User.adminName' instead?
