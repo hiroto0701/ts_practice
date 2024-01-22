05ab2a63d5a470dbcef96dbf85fbac5d872b8b07
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-22 23:46:42 +0900

    privateは外部からアクセスできない。protectedはクラス自身、もしくは継承した場合はアクセス可能した場合はアクセス可能

--- src/sec05.ts
+++ src/sec05.ts
@@ -56,56 +56,56 @@
 // console.log(me.name, me.age, me.isMen, me.isAdult());  // hiroto 25 true
 
 /* 5.1.5 静的プロパティ、静的メソッド */
-// staticはインスタンスではなく、クラスそのものに属するプロパティ・メソッドのこと
+// // staticはインスタンスではなく、クラスそのものに属するプロパティ・メソッドのこと
 
-// Userクラスは staticな変数（adminName）と staticな関数（getAdminUser()）を持っている
-class User {
-  static adminName: string = "inagaki";
-  static getAdminUser() {
-    return new User(User.adminName, 25);
-  }
-
-  name: string;
-  age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-
-  isAdult(): boolean {
-    return this.age >= 20;
-  } 
-}
-
-// インスタンスを作成するのではなく、直接UserクラスのstaticなプロパティとメソッドにアクセスするのはOK
-console.log(User.adminName);
-const admin = User.getAdminUser();
-console.log(admin.age);
-console.log(admin.name);
-
-// インスタンスを作成して、staticなプロパティや、メソッドにアクセスするのはNG
-const hiro = new User("hiro", 25);
-console.log(hiro.adminName);  // Property 'adminName' does not exist on type 'User'. Did you mean to access the static member 'User.adminName' instead?
-// staticのメリットはインスタンスを生成せずにプロパティやメソッドにアクセスできる点
-
-/* 5.1.6 3種類のアクセシビリティ修飾子 */
-// // public private protectedの3種類。アクセシビリティ修飾子といいます。
+// // Userクラスは staticな変数（adminName）と staticな関数（getAdminUser()）を持っている
 // class User {
+//   static adminName: string = "inagaki";
+//   static getAdminUser() {
+//     return new User(User.adminName, 25);
+//   }
+
 //   name: string;
-//   private age: number;
+//   age: number;
 
 //   constructor(name: string, age: number) {
 //     this.name = name;
 //     this.age = age;
 //   }
 
-//   public isAdult() {
+//   isAdult(): boolean {
 //     return this.age >= 20;
-//   }
+//   } 
 // }
+
+// // インスタンスを作成するのではなく、直接UserクラスのstaticなプロパティとメソッドにアクセスするのはOK
+// console.log(User.adminName);
+// const admin = User.getAdminUser();
+// console.log(admin.age);
+// console.log(admin.name);
+
+// // インスタンスを作成して、staticなプロパティや、メソッドにアクセスするのはNG
 // const hiro = new User("hiro", 25);
-// console.log(hiro.name);
-// console.log(hiro.isAdult());
-// // console.log(hiro.age); // Property 'age' is private and only accessible within class 'User'. 
-// // private で定義されているプロパティ、メソッドはクラスの外からアクセスできない
+// console.log(hiro.adminName);  // Property 'adminName' does not exist on type 'User'. Did you mean to access the static member 'User.adminName' instead?
+// staticのメリットはインスタンスを生成せずにプロパティやメソッドにアクセスできる点
+
+/* 5.1.6 3種類のアクセシビリティ修飾子 */
+// public private protectedの3種類。アクセシビリティ修飾子といいます。
+class User {
+  name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public isAdult() {
+    return this.age >= 20;
+  }
+}
+const hiro = new User("hiro", 25);
+console.log(hiro.name);
+console.log(hiro.isAdult());
+// console.log(hiro.age); // Property 'age' is private and only accessible within class 'User'. 
+// private で定義されているプロパティ、メソッドはクラスの外からアクセスできない
